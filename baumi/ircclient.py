eafdda95e27643bd5f3123aff3dd9e0e80aff043
acoolon <acoolon@web.de>
2011-05-15 17:23:56 +0200

    fixed has_op_voice (nick == user)

--- baumi/ircclient.py
+++ baumi/ircclient.py
@@ -236,6 +236,7 @@ class IRCClient(asynsocket.asynchat):
         logger.exception('Exception in IRCClient')
 
     def has_op_voice(self, channel, nick):
+        if channel == nick: return False
         channel = self.protocol.channels[channel]
         return channel.has_op(nick) or channel.has_voice(nick)
 
